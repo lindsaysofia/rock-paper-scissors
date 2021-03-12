a95204eea2d33d9b1600e9a4ad953c7fe6a666f6
Lindsay Lopez <lopez95@berkeley.edu>
2021-03-11 20:39:57 -0800

    created playRound function

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function computerPlay() {
   let randomMove = moves[randomIndex];
   return randomMove;
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+  switch(true) {
+    case (playerSelection === 'rock' && computerSelection === 'scissors'):
+    case (playerSelection === 'paper' && computerSelection === 'rock'):
+    case (playerSelection === 'scissors' && computerSelection === 'paper'):
+      return 1;
+    case (playerSelection === 'rock' && computerSelection === 'paper'):
+    case (playerSelection === 'paper' && computerSelection === 'scissors'):
+    case (playerSelection === 'scissors' && computerSelection === 'rock'):
+      return 0;
+    default:
+      return 2;      
+  }
+}
